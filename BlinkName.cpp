8b1f6c4d1cb75489f54325cddfeb1330aeb239b5
KasparByrne <103092736+KasparByrne@users.noreply.github.com>
2022-04-06 14:33:21 +1000

    Update BlinkName.cpp

--- BlinkName.cpp
+++ BlinkName.cpp
@@ -1,19 +1,22 @@
-int led1 = D7;
+int led1 = D7; //select embedded LED
 int morse[10] = {1,3,1,1,1,1,1,3,3,1}; //ethan : . - .... .- -.
 
+//Initialise
 void setup() {
 
   pinMode(led1, OUTPUT);
 
 }
 
+//Loop run on argon
 void loop() {
   //dots = 500ms, dash = 1500ms
-  for (int i = 0; i < 10; i++) {
-    digitalWrite(led1, HIGH);
-    delay(morse[i]*500);
-    digitalWrite(led1, LOW);
-    delay(500);   
+  for (int i = 0; i < 10; i++) { //iterate through morse[]
+    digitalWrite(led1, HIGH); //turn on LED
+    delay(morse[i]*500); //wait 500*i miliseconds
+    digitalWrite(led1, LOW); //turn off LED
+    delay(500); //Wait 500 miliseconds   
   }
-  delay(3000);
+  delay(3000); //wait 3 seconds
+  //repeat
 }
