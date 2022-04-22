c70d60508177ed3a7086c9d4e9e9e26a21853321
KasparByrne <103092736+KasparByrne@users.noreply.github.com>
2022-04-23 01:19:00 +1000

    Update BlinkName.cpp

--- BlinkName.cpp
+++ BlinkName.cpp
@@ -1,5 +1,27 @@
+#include <string>
+using namespace std;
+
 int led1 = D7; //select embedded LED
 int morse[10] = {1,3,1,1,1,1,1,3,3,1}; //ethan : . - .... .- -.
+string alpha[26] = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J",
+                    "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T",
+                    "U", "V", "W", "X", "Y", "Z"};
+string morseCode = {".-", "-...", "-.-.", "-..",".", "..-.", "--.", "....", "..", ".---",
+                   "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-",
+                   "..-", "...-", ".--", "-..-", "-.--", "--.."}; //letters in morse
+
+string convertToMorse(string word) {
+  string translation = "";
+  for (auto &letter : word) {
+    for (int i; i < 26; i++) {
+      if (alpha[i] == letter) {
+        translation += alpha[i]
+        break;
+      }
+    }
+  }
+  return translation;
+}
 
 //Initialise
 void setup() {
