54ff81e5f818a1272c8d76d1d489195c5587510a
KasparByrne <103092736+KasparByrne@users.noreply.github.com>
2022-04-27 14:16:31 +1000

    Update BlinkName.cpp

--- BlinkName.cpp
+++ BlinkName.cpp
@@ -1,44 +1,66 @@
-#include <string>
-using namespace std;
-
+String blinkMorse = "";
 int led1 = D7; //select embedded LED
-int morse[10] = {1,3,1,1,1,1,1,3,3,1}; //ethan : . - .... .- -.
-string alpha[26] = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J",
-                    "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T",
-                    "U", "V", "W", "X", "Y", "Z"};
-string morseCode = {".-", "-...", "-.-.", "-..",".", "..-.", "--.", "....", "..", ".---",
+int tickTime = 500; //base time for morse blink
+String phrase = "ethan"; //ethan : . - .... .- -.
+
+char alpha[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+                    'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+                    'u', 'v', 'w', 'x', 'y', 'z'}; //alphabet
+String morseCode[26] = {".-", "-...", "-.-.", "-..",".", "..-.", "--.", "....", "..", ".---",
                    "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-",
                    "..-", "...-", ".--", "-..-", "-.--", "--.."}; //letters in morse
 
-string convertToMorse(string word) {
-  string translation = "";
-  for (auto &letter : word) {
-    for (int i; i < 26; i++) {
-      if (alpha[i] == letter) {
-        translation += alpha[i]
-        break;
-      }
+//translate phrase to morse
+String transToMorse(String phrase)
+{
+    String translation = "";
+    for (int i = 0; i < phrase.length(); i++) //iterate through letters of phrase
+    {
+        for (int j = 0; j < 26; j++) //iterate through alphabet array
+        {
+            if (phrase[i] == alpha[j]) 
+            {
+                translation += morseCode[j]; //add morse to translation
+                break;
+            }
+        }
+        
     }
-  }
-  return translation;
+    return translation;
+}
+
+//blink morse code
+void morseToBlink(String blinkMorse) {
+    for (int i = 0; i < blinkMorse.length(); i++) 
+    {
+        digitalWrite(led1, HIGH); //turn on LED
+        //dot, 1 tick; dash, 3 ticks;
+        if (blinkMorse[i] == '.') 
+        {
+            delay(tickTime);
+        }
+        else 
+        {
+            delay(tickTime*3);
+        }
+        digitalWrite(led1, LOW); //turn off LED
+        delay(tickTime); //Wait wait standard time
+    }
 }
 
 //Initialise
 void setup() {
 
   pinMode(led1, OUTPUT);
+  blinkMorse = transToMorse(phrase);
 
 }
 
 //Loop run on argon
 void loop() {
   //dots = 500ms, dash = 1500ms
-  for (int i = 0; i < 10; i++) { //iterate through morse[]
-    digitalWrite(led1, HIGH); //turn on LED
-    delay(morse[i]*500); //wait 500*i miliseconds
-    digitalWrite(led1, LOW); //turn off LED
-    delay(500); //Wait 500 miliseconds   
-  }
+  morseToBlink(blinkMorse);
+  
   delay(3000); //wait 3 seconds
   //repeat
 }
